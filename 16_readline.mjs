52cf243a9464f40c19c972a659b556683e26d392
IDrumahdev <ibnudirsan@gmail.com>
2023-09-22 13:20:45 +0700

    readline

---
+++ 16_readline.mjs
@@ -0,0 +1,14 @@
+import readline from 'readline';
+import process from 'process';
+
+const input = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout 
+});
+
+input.question("Siapa nama anda? ", (name) => {
+    console.info(`Hallo ${name}`);
+    input.close();
+});
+
+// node 16_readline.mjs
