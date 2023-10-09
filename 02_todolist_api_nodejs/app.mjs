1e599626af7698b2f3b7ddfbf2147e2871985765
IDrumahdev <ibnudirsan@gmail.com>
2023-10-09 15:59:14 +0700

    CRUD Todo List Nodejs

--- 02_todolist_api_nodejs/app.mjs
+++ 02_todolist_api_nodejs/app.mjs
@@ -1,8 +1,21 @@
 import http from 'http';
+import { TodoListService } from './todolist-service.mjs';
 
+const service = new TodoListService();
 const server = http.createServer((request, response) => {
-    response.write("Todo List API");
-    response.end();
+
+    response.setHeader("Content-Type" , "application/json");
+    
+    if(request.method === "GET") {
+        service.getTodolist(request, response);
+    } else if (request.method === "POST") {
+        service.createTodo(request, response);
+    } else if (request.method === "PUT") {
+        service.updateTodo(request, response);
+    } else if (request.method === "DELETE") {
+        service.deleteTodo(request, response);
+    }
+
 });
 
 server.listen(3300);
