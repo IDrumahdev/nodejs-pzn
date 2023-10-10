9671a05d393a6d57b34d60577848f0c8b4d1cd54
IDrumahdev <ibnudirsan@gmail.com>
2023-10-10 14:35:38 +0700

    update dependency

--- 03_npm/package.js
+++ 03_npm/package.js
@@ -1,7 +1,10 @@
 import {sayHello, sum} from 'rumahdev-librarypzn';
+import {min, max} from 'rumahdev-librarypzn/number';
 
 console.info(sayHello("Heri"));
 
 const numbers = [10,20,30,40,50];
-
 console.info(sum(numbers));
+
+console.info(min(40, 35));
+console.info(max(30, 45));
