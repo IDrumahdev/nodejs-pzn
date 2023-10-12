7f2a52b36653d741b88571f6025d536c9073f904
IDrumahdev <ibnudirsan@gmail.com>
2023-10-12 16:32:07 +0700

    mock function

--- 05_unit_test/src/sum.js
+++ 05_unit_test/src/sum.js
@@ -9,3 +9,19 @@ export const sumAll = (numbers) => {
     }
         return total;
 }
+
+export const calculate = (numbers, callback) => {
+    let total = 0;
+    for (const number of numbers) {
+        total += number;
+    }
+        callback(total);
+}
+
+export const caculateAndReturn = (numbers, callback) => {
+    let total = 0;
+    for (const number of numbers) {
+        total += number
+    }
+        return callback(total);
+}
