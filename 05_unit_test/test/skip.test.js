04d44e89b37221a39d0736faf8fde34cc07dbf44
IDrumahdev <ibnudirsan@gmail.com>
2023-10-12 07:17:43 +0700

    skip test

---
+++ 05_unit_test/test/skip.test.js
@@ -0,0 +1,5 @@
+test("test satu", () => console.info("Test satu"));
+test("test dua", () => console.info("Test dua"));
+test.skip("test tiga", () => console.info("Test tiga"));
+test("test empat", () => console.info("Test empat"));
+test("test lima", () => console.log("Test lima"));
