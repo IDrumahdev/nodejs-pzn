2c0a77c0d83f1fd1b9e34dcb2519acf6f6ae84b6
IDrumahdev <ibnudirsan@gmail.com>
2023-10-13 15:28:00 +0700

    logger format combine

---
+++ 06_logger/test/logger-combine-format.test.js
@@ -0,0 +1,25 @@
+import winston from 'winston';
+
+test("create new logger format combine", () => {
+    
+    const logger = winston.createLogger({
+        level: "silly",
+        format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            winston.format.json(),
+        ),
+        transports: [
+            new winston.transports.Console({})
+        ]
+    });
+
+    logger.error("Hello Error");
+    logger.warn("Hello Warn");
+    logger.info("Hello Info");
+    logger.http("Hello HTTP");
+    logger.verbose("Hello Verbose");
+    logger.debug("Hello Debug");
+    logger.silly("Hello Silly");
+
+});
