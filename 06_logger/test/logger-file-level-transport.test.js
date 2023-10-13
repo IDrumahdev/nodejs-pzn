d2641d04886c4b93a79be851cf842585a33eb5ad
IDrumahdev <ibnudirsan@gmail.com>
2023-10-13 15:43:27 +0700

    level file transport

--- 06_logger/test/logger-file-level-transport.test.js
+++ 06_logger/test/logger-file-level-transport.test.js
@@ -12,7 +12,12 @@ test("create new logger file transport", () => {
         transports: [
             new winston.transports.Console({}),
             new winston.transports.File({
+                level: "info",
                 filename: "application.log"
+            }),
+            new winston.transports.File({
+                level: "error",
+                filename: "application-error.log"
             })
         ]
     });
