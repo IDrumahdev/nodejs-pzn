d980c9181c8658eaa1a6ca9fb060c109bd51de28
IDrumahdev <ibnudirsan@gmail.com>
2023-10-14 07:38:25 +0700

    Basic Routing

--- 07_expressjs_pemula/src/hello.js
+++ 07_expressjs_pemula/src/hello.js
@@ -2,6 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (request, response) => {
+    response.send('Hello Word');
+});
+
+app.get('/ibnudirsan', (request, response) => {
+    response.send('Hello Ibnudirsan');
+});
+
 app.listen(3100, () => {
     console.log(`Server Started on Port 3100`);
 });
