2f5d197161deccd88d10c12a6c095da57c33c431
IDrumahdev <ibnudirsan@gmail.com>
2023-10-20 13:18:47 +0700

    Prisma Client

---
+++ 09_nodejs_database_prisma_orm/test/prisma-client.test.js
@@ -0,0 +1,11 @@
+import {PrismaClient} from '@prisma/client';
+
+describe("Prisma Clinet", () => {
+    it('should be able to connect', async () => {
+        const prisma = new PrismaClient();
+        await prisma.$connect();
+
+        // do something
+        await prisma.$disconnect();
+    });
+})
