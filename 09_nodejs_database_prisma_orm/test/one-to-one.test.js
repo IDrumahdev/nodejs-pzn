25a81c8e83ca1a9760ce3a4c4a1f250e6649d0c0
IDrumahdev <ibnudirsan@gmail.com>
2023-11-04 04:03:57 +0700

    find one in relasi one to one

--- 09_nodejs_database_prisma_orm/test/one-to-one.test.js
+++ 09_nodejs_database_prisma_orm/test/one-to-one.test.js
@@ -15,4 +15,39 @@ describe('Prisma Clinet Relasi', () => {
 
         console.info(wallet);
     });
+
+    it('create customer new and new wallet one to one relasi', async () => {
+        const customer = await prismaClient.customer.create({
+            data: {
+                id: "d5c585fe-7a89-11ee-b962-0242ac120002",
+                name:"fulan",
+                email:"fulan@example.com",
+                phone: "0090708908122",
+                wallet: {
+                    create: {
+                        id:"d5c58734-7a89-11ee-b962-0242ac120002",
+                        balance: 212000
+                    }
+                }
+            },
+            include: {
+                wallet: true
+            }
+        });
+
+        console.info(customer);
+    });
+
+    it('find one in relasi one to one', async () => {
+        const customer = await prismaClient.customer.findUnique({
+            where: {
+                id: "2530429c-79f8-11ee-b962-0242ac120002"
+            },
+            include: {
+                wallet: true
+            }
+        });
+
+        console.info(customer);
+    });
 });
