eadbeaece0cbf92e8ec1ccc9e50c26024e997a4f
IDrumahdev <ibnudirsan@gmail.com>
2023-11-08 08:45:44 +0700

    validation object

--- 10_nodejs_validation/test/object-validation.test.js
+++ 10_nodejs_validation/test/object-validation.test.js
@@ -19,4 +19,36 @@ describe('Validation Nodejs', () => {
         console.info(result);
         
     });
+
+    it('Netsed Object Validation', () => {
+        const createUserSchema = Joi.object({
+            id: Joi.string().required().max(100),
+            name: Joi.string().required().max(100),
+            address: Joi.object({
+                street: Joi.string().required().max(200),
+                city: Joi.string().required().max(100),
+                country: Joi.string().required().max(100),
+                zipCode: Joi.string().required().max(10)
+            }).required()
+        });
+
+        const request = {
+            address: {
+
+            }
+        };
+
+        const result = createUserSchema.validate(request, {
+            abortEarly: false
+        });
+
+        console.info(result);
+
+        if(result.error) {
+            result.error.details.forEach(detail => {
+                console.info(`${detail.path} = ${detail.message}`);
+            });
+        }
+
+    });
 });
