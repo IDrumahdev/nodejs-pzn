9d3f66fa6c4c2c6de85d0cc530b1265b0c693e5f
IDrumahdev <ibnudirsan@gmail.com>
2023-11-10 17:40:50 +0700

    register new user

--- 11_restful_api/test/user.test.js
+++ 11_restful_api/test/user.test.js
@@ -12,6 +12,7 @@ describe('POST /api/users', () => {
         });
     })
 
+    // npx jest test/user.test.js -t "register new user"
     it('register new user', async () => {
         const result = await supertest(web)
             .post('/api/users')
@@ -25,5 +26,41 @@ describe('POST /api/users', () => {
         expect(result.body.data.username).toBe("ibnudirsan");
         expect(result.body.data.name).toBe("Heri Purwanto");
         expect(result.body.data.password).toBeUndefined();
+    });
+
+    // npx jest test/user.test.js -t "reject if request is invalid"
+    it('reject if request is invalid', async () => {
+        const result = await supertest(web)
+            .post('/api/users')
+            .send({
+                username: "",
+                password: "",
+                name: ""
+            });
+        
+        expect(result.status).toBe(400);
+        expect(result.body.errors).toBeDefined();
+    });
+
+    // npx jest test/user.test.js -t "reject if request is already registered"
+    it('reject if request is already registered', async () => {
+        let result = await supertest(web)
+            .post('/api/users')
+            .send({
+                username: "ibnudirsan",
+                password: "rahasia",
+                name: "Heri Purwanto"
+            });
+
+        result = await supertest(web)
+            .post('/api/users')
+            .send({
+                username: "ibnudirsan",
+                password: "rahasia",
+                name: "Heri Purwanto"
+            });
+        
+        expect(result.status).toBe(400);
+        expect(result.body.errors).toBeDefined();
     });
 });
