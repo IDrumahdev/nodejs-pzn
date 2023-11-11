63fa1b4c63559bd904f10e4377019c2c2d0fa669
IDrumahdev <ibnudirsan@gmail.com>
2023-11-11 17:23:34 +0700

    create contact

--- 11_restful_api/test/contact.test.js
+++ 11_restful_api/test/contact.test.js
@@ -13,6 +13,7 @@ describe('POST /api/contacts', () => {
         await removeTestUser();
     });
 
+    // npx jest test/contact.test.js -t "create contact"
     it('create contact', async () => {
         const result = await supertest(web)
             .post('/api/contacts')
@@ -31,6 +32,23 @@ describe('POST /api/contacts', () => {
                 expect(result.body.data.email).toBe("test@rumahdev.pro");
                 expect(result.body.data.phone).toBe("08182721901");
     });
+
+    // npx jest test/contact.test.js -t "create contact reject if data invalid"
+    it('create contact reject if data invalid', async () => {
+        const result = await supertest(web)
+            .post('/api/contacts')
+            .set('Authorization', 'test')
+            .send({
+                first_name: "",
+                last_name: "test",
+                email: "test",
+                phone: "08182721901009079889689686786876869869"
+            });
+
+                expect(result.status).toBe(400);
+                expect(result.body.errors).toBeDefined();
+    });
+
 });
 
 
